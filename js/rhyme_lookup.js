8ab58c8fba629c0881354b8fc16d9a7284c3f75d
Sam Choi <sam.choi@berkeley.edu>
2016-06-12 00:32:54 -0700

    added original js files

---
+++ js/rhyme_lookup.js
@@ -0,0 +1,84 @@
+var mobytrie = new Trie();
+
+// Populate the mobypron trie [with pronunciations]
+for (var mobyword in mobypron) {
+    if (mobypron.hasOwnProperty(mobyword)) {
+        var fields = mobypron[mobyword].split(/\/+/);
+        mobytrie.insert(fields, mobyword);
+    }
+}
+
+// Populate a second trie with the words themselves
+var wordTrie = new Trie();
+
+var words = Object.keys(mobypron);
+var numWords = words.length;
+
+for (var i = 0; i < numWords; i++) {
+    var word = words[i];
+    wordTrie.insert(word.split(''), word);
+}
+
+/* 
+ * Returns a set of words from the mobypron trie that (hopefully)
+ * rhyme with the word specified by PRON_FIELDS.
+ */
+var fromTrie = function(pronFields) {
+    if (pronFields.length > 4) {
+        pronFields.splice(0, pronFields.length - 3);
+    } else if (pronFields.length > 2) {
+        pronFields.splice(0, pronFields.length - 2);
+    }
+    
+    var results = mobytrie.autoComplete(pronFields);
+    if (results.length == 0) {
+        return ["grandfather clock"]; // hey, we've got to return something!
+    }
+    
+    // Return a random word from results
+    return [results[Math.floor(Math.random() * results.length)]];
+}
+
+/*
+ * Like fromTrie, but with the additional guarantee 
+ * that an 's' will automatically be appended to the resulting rhyme.
+ */
+var pluralFromTrie = function(pronFields) {
+    var rhyme = fromTrie(pronFields);
+    rhyme[0] = rhyme[0].concat("s");
+    return rhyme;
+}
+
+var lookup = function(word) {
+    if (word.slice(-1) === "*") {
+        switch (word) {
+            case "n****":
+                return ["gold digga"];
+            case "f***":
+                return ["luck"];
+            case "s***":
+                return ["hit"];
+        }
+    } else if (mobypron.hasOwnProperty(word)) {
+        return fromTrie(mobypron[word].split(/\/+/));
+    } else if (mobypron.hasOwnProperty(word.toLowerCase())) {
+        return fromTrie(mobypron[word.toLowerCase()].split(/\/+/));
+    } else {
+        var singular = word.substring(0, word.length - 1);
+        if (word.slice(-1) === "s" && mobypron.hasOwnProperty(singular)) {
+            return pluralFromTrie(mobypron[singular].split(/\/+/));
+        }
+        
+        var approximation = wordTrie.getApproximation(word.toLowerCase().split(''));
+        if (approximation == null && word.slice(-1) === "s") { // plurality case
+            approximation = wordTrie.getApproximation(singular.split(''));
+            if (approximation == null) {
+                return ["grandmother clocks"];
+            } else {
+                return pluralFromTrie(mobypron[approximation].split(/\/+/));
+            }
+        }
+        
+        return fromTrie(mobypron[approximation].split(/\/+/));
+    }
+};
---
+++ js/trie.js
@@ -0,0 +1,120 @@
+/**
+ * A trie. Based on an implementation written by Saurabh Odhyan (http://odhyan.com).
+ * That implementation can be found here: http://odhyan.com/blog/2010/11/trie-implementation-in-javascript/.
+ * Optimized for IPA field insertion and reverse string lookup.
+ * 
+ * Author: Owen Jow
+ * Date: Oct 11, 2015 [shoutout to Cal Hacks!]
+ */
+
+function Trie() {
+    this.word = null; // the word we've got at this point in the trie
+    this.children = {};
+}
+
+Trie.prototype = {
+    
+    /*
+     * Inserts an IPA entry into the trie (backwards).
+     * Recursively traverses the trie, creating new nodes wherever they don't already exist.
+     *
+     * @method insert
+     * @param {Array} ipaEntries IPA fields to insert [should be split on /, i.e. w/ str.split()]
+     * @param {String} word The word that the overall IPA phrase is actually associated with
+     * @return {Void}
+     */
+    insert: function(ipaEntries, word) {
+        if (ipaEntries.length == 0) {
+            // "It's over! It's all over!"
+            this.word = word;
+            return;
+        }
+        
+        var field = ipaEntries[ipaEntries.length - 1]; // should be an IPA field str
+        if (this.children[field] === undefined) {
+            this.children[field] = new Trie();
+        }
+        
+        ipaEntries.pop();
+        this.children[field].insert(ipaEntries, word);
+    },
+    
+    /*
+     * Gets all of the words in the trie.
+     */
+    getAllWords: function() {
+        var words = [], 
+            c;
+            
+        if (this.word != null) {
+            words.push(this.word);
+        }
+        
+        for (c in this.children) {
+            words = words.concat(this.children[c].getAllWords());
+        }
+        
+        return words;
+    },
+    
+    /*
+     * Gets a single word from the trie (the first one it finds).
+     */
+    getOneWord: function() {
+        if (this.word != null) {
+            return this.word;
+        } else {
+            var word = null;
+            for (c in this.children) {
+                word = this.children[c].getOneWord();
+                if (word != null) {
+                    break;
+                }
+            }
+            
+            return word;
+        }
+    },
+    
+    /*
+     * Autocompletes the given suffix.
+     * 
+     * @param {Array} fields An array of IPA fields to serve as a suffix (in order!)
+     * @return {Array} Array of possible suggestions
+     */
+    autoComplete: function(fields) {
+        if (fields.length == 0) {
+            return this.getAllWords();
+        }
+        
+        child = this.children[fields[fields.length - 1]];
+        if (child === undefined) {
+            return [];
+        } else if (fields.length === 1) {
+            return child.getAllWords();
+        } else {
+            fields.pop();
+            return child.autoComplete(fields);
+        }
+    },
+    
+    /*
+     * Returns an approximation of the given word.
+     * It is assumed that the word does not already exist in the trie;
+     * thus, this function works by following the characters of the word
+     * (starting from the end) through the trie – and when it reaches a dead end,
+     * it returns an approximation from the set of all of the dead end node's children.
+     * 
+     * @param {Array} charArray An array of characters to follow
+     * @return {String} An approximation of the word specified by CHAR_ARRAY
+     */
+    getApproximation: function(charArray) {
+        child = this.children[charArray[charArray.length - 1]];
+        if (child === undefined) {
+            return this.getOneWord();
+        } else {
+            charArray.pop();
+            return child.getApproximation(charArray);
+        }
+    }
+};
